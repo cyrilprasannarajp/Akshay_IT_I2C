f3cf5afd8ecc79d3698483286863ff8e5f0d7197
Kamales2005 <kamales5468@gmail.com>
2026-06-01 23:19:19 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,7 +2,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_project(dut):
 
@@ -12,16 +11,18 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.rst_n.value = 0
     dut.ui_in.value = 0
+    dut.uio_in.value = 0
 
     await ClockCycles(dut.clk, 5)
 
     dut.rst_n.value = 1
 
-    # Start transmission
-    dut.ui_in.value = 0xA5
+    # Start transfer
+    dut.ui_in.value = 8'hA5
 
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
 
-    assert dut.uo_out.value & 1
+    # BUSY bit is uo_out[0]
+    assert int(dut.uo_out.value) != 0
 
     await ClockCycles(dut.clk, 20)
